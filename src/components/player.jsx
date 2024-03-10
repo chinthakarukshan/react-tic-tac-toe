c06a6993c8e350f08304172b54da6f4e7a69958a
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-03-10 22:32:18 +0530

    Change the button label dynamically and make the player name field editable

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+
 function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(() => false);
+
+    let buttonLabel = "Edit";
+    let playerNameField = <span className="player-name">{name}</span>;
+
+    if (isEditing) {
+        buttonLabel="Save";
+        playerNameField = <input type="text" required value={name}/>
+    }
+
+    function handleEditClick() {
+        setIsEditing(!isEditing);
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {playerNameField}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{buttonLabel}</button>
         </li>
     );
+
 }
+
+
 
 export default Player;
