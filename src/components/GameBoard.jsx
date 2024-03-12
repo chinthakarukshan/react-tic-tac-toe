193d835aea07e998274ae9f15b4f7c630e2fac1e
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-03-12 23:30:23 +0530

    Enhancement to update the button upon mouse click

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,12 +7,23 @@ const initialBoard = [
 ];
 
 export default function GameBoard() {
+
+    const [updatedGameBoard, setUpdatedGameBoard] = useState(initialBoard);
+
+    function handleUpdateGameboard(rowIndex, colIndex) {
+        setUpdatedGameBoard((existingGameBoard) => {
+            const newGameBoard = [...existingGameBoard.map((gameBoardRow) => [...gameBoardRow])]
+            newGameBoard[rowIndex][colIndex] = 'X';
+            return newGameBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialBoard.map((row, rowIndex) => (<li key={rowIndex}>
+            {updatedGameBoard.map((row, rowIndex) => (<li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, rowIndex) => (<li key={rowIndex}>
-                        <button>{playerSymbol}</button>
+                    {row.map((playerSymbol, colIndex) => (<li key={colIndex}>
+                        <button onClick={() => handleUpdateGameboard(rowIndex,colIndex)}>{playerSymbol}</button>
                     </li>))}
                 </ol>
             </li>))}
